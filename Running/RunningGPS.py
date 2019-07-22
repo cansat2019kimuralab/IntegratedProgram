a729b2d4736a468184db05c833bdaa8e0d11fd01
masatoshi_moto <45708319+masatoshiMoto@users.noreply.github.com>
2019-07-22 13:28:51 +0900

    20190722

--- Running/RunningGPS.py
+++ Running/RunningGPS.py
@@ -17,7 +17,7 @@ import Other
 
 fileCal = "" 						#file path for Calibration
 ellipseScale = [0.0, 0.0, 0.0, 0.0] #Convert coefficient Ellipse to Circle
-disGoal = 0.0						#Distance from Goal [m]
+disGoal = 100.0						#Distance from Goal [m]
 angGoal = 0.0						#Angle toword Goal [deg]
 angOffset = -83.0					#Angle Offset towrd North [deg]
 gLat, gLon = 35.918181, 139.907992	#Coordinates of That time
@@ -25,7 +25,13 @@ nLat, nLon = 0.0, 0.0		  		#Coordinates of That time
 nAng = 0.0							#Direction of That time [deg]
 relAng = 0.0						#Relative Direction between Goal and Rober That time [deg]
 mP = 0								#Motor Power
-gpsInterval							#GPS Log Interval Time
+gpsInterval = 0						#GPS Log Interval Time
+
+gpsData = [0.0, -1.0, -1.0, 0.0, 0.0]						#variable to store GPS data
+bmx055data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]	#variable to store BMX055data
+
+runningLog = "runningLog.txt"
+calibrationLog = "calibrationLog"
 
 pi = pigpio.pi()	#object to set pigpio
 
@@ -48,46 +54,54 @@ if __name__ == "__main__":
 		setup()
 		time.sleep(1)
 
-		fileCal = "cal_t"
-		fileCal = Other.fileName(fileCal, "txt")
-
+		fileCal = Other.fileName(calibrationLog, "txt")
+		
 		Calibration.readCalData(fileCal)
 		ellipseScale = Calibration.Calibration(fileCal)
 
 		gpsInterval = 0
-		while 1:
+
+		#Get GPS data
+		while(gpsData[1] == -1.0 and gpsData[2] == 0.0):
+			gpsData = GPS.readGPS()
+
+		while disGoal >= 5:
 			gpsInterval = gpsInterval + 1
-			if(gpsInterval = 300):
-				while(gpsdfata[1] = 0.0 and gpsData[3] = 0.0):
+			if(gpsInterval == 300):
+				while(gpsData[1] == -1.0 and gpsData[2] == 0.0):
 						gpsData = GPS.readGPS()
-						nLat = gpsData[1]
-						
 				gpsInterval = 0
 
+			#Calculate angle
 			bmx055Data = BMX055.bmx055_read()
-			nAng = Calibration.readDir(ellipseScale) - angOffset
+			nAng = Calibration.readDir(ellipseScale, bmx055Data) - angOffset
 			nAng = nAng if nAng <= 180.0 else nAng - 360.0
 			nAng = nAng if nAng >= -180.0 else nAng + 360.0
-			if(gpsData[1] != 0.0 and gpsData[2] != 0.0):
-				nLat = gpsData[1]
-				nLon = gpsData[2]
-				disGoal, angGoal = GPS.Cal_RhoAng(nLat, nLon, gLat, gLon)
-				relAng = angGoal - nAng
-				relAng = relAng if relAng <= 180 else relAng - 360
-				relAng = relAng if relAng >= -180 else relAng + 360
-				'''
-				with open("log.txt", "a") as f:
-					f.write(str(nLat) + "\t" + str(nLon) + "\t" + str(disGoal) + "\t" + str(angGoal) + "\n")
-				'''
+			
+			#Calculate disGoal and relAng
+			nLat = gpsData[1]
+			nLon = gpsData[2]
+			disGoal, angGoal = GPS.Cal_RhoAng(nLat, nLon, gLat, gLon)
+			relAng = angGoal - nAng
+			relAng = relAng if relAng <= 180 else relAng - 360
+			relAng = relAng if relAng >= -180 else relAng + 360
+			'''
+			with open("log.txt", "a") as f:
+				f.write(str(nLat) + "\t" + str(nLon) + "\t" + str(disGoal) + "\t" + str(angGoal) + "\n")
+			'''
 
-
+			#Calculate Motor Power
 			mP = int(relAng * 1.0)
 			mP = 30 if mP > 30 else mP
 			mP = -30 if mP < -30 else mP
 			#Motor.motor(mP, -mP, 0.001, 1)
 
 			print(gpsData[1], gpsData[2], disGoal, angGoal, nAng, relAng, mP)
-			time.sleep(1)
+			
+			IM920.Send(str(nLat) + ":" + tr(nLon))
+			time.sleep(0.1)
+
+		print("Switch to Goal Detection")
 		close()
 	except KeyboardInterrupt:
 		close()
