9b87c3c112f40375500954edddf364509f919de9
arai <s-arai14@jcom.zaq.ne.jp>
2019-07-26 00:50:01 +0900

    21090726

--- Goal/Goal.py
+++ Goal/Goal.py
@@ -10,38 +10,81 @@ import binascii
 import goal_detection
 import Motor
 
-def Togoal():
-    L, GAP, photoname = goal_detection.GoalDetection("photo")
-    if L == 0 and GAP == 0 :
-        Motor.motor(0,0,0.3)
-        return 0
-    
-    elif L == -1 and GAP == -1 :
-        Motor.motor(-50,50,0.3)
-        return -1
+def Togoal(photopath, H_min, H_max, S_thd):
+	L, GAP, photoname = goal_detection.GoalDetection(photopath, H_min, H_max, S_thd)
+	if L == 0 and GAP == 0:
+		Motor.motor(0, 0, 0.3)
+		return 0
+	
+	elif L == -1 and GAP == -1:
+		Motor.motor(-50, 50, 0.3)
+		Motor.motor(0, 0, 0.3)
+		return -1
 
-    else :
-        if GAP > 0:
-		    #goal right
-            Motor.motor(50,20,0.3)
-            return 1
+	else:
+		speed = SpeedSwitch(L)
+		mPL, mPR, switch = CurvingSwitch(GAP, speed)
+		Motor.motor(mPL, mPR, 1)
+		Motor.motor(0, 0, 0.3)
+		return switch
 
-        elif GAP < 0:
-		    #gaol left
-            Motor.motor(20,50,0.3)
-            return 2
+def SpeedSwitch(L):
+	if L > 10:
+		return 70
+	elif L > 5:
+		return 50
+	else:
+		return 30 
 
-        else :
-            #goal center
-            Motor.motor(30,30,0.3)
-            return 3
+def CurvingSwitch(GAP, speed):
+	if GAP == 0:
+		return [speed, speed, 3]
+	elif abs(GAP) < 40:
+		RATE = 1
+		rate = 0.8
+	elif abs(GAP) < 80:
+		RATE = 1
+		rate = 0.7
+	elif abs(GAP) < 120:
+		RATE = 1
+		rate = 0.6
+	else:
+		RATE = 1
+		rate = 0.5
+	if GAP > 0:
+		mPLeft = speed * RATE
+		mPRight = speed * rate
+
+	else:
+		mPLeft = speed * rate
+		mPRight = speed * RATE
+		return [mPLeft, mPRight, 2]
+
+	return [mPLeft, mPRight, 1]
 
 if __name__ == "__main__":
-    try:
-        goal = Togoal()
-        while goal != 0:
-            print(goal)
-            goal = Togoal()
-    except KeyboardInterrupt:
-        print("Emergency!")
-        Motor.motor_stop()
+	try:
+		count = 0
+		ahh = 0
+		H_min = 200
+		H_max = 10
+		S_thd = 120
+		goal = Togoal("photo", H_min, H_max, S_thd)
+		while goal != 0:
+			while count < 10:
+				print(goal)
+				goal = Togoal("photo", H_min, H_max, S_thd)
+				if goal ==-1:
+					count = count + 1
+			if count == 10:
+				H_min = H_min - 5
+				H_max = H_max + 5
+				count = 0
+				ahh = ahh + 1
+			if ahh == 5:
+				print("runningGPS again")
+				break
+
+	except KeyboardInterrupt:
+		print("Emergency!")
+		Motor.motor_stop()
