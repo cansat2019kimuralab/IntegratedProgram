ec2ce686929bd767b72e036c55ced68b0b60f184
unknown <s-arai14@jcom.zaq.ne.jp>
2019-07-30 13:43:50 +0900

    20190730

--- Stuck/spin.py
+++ Stuck/spin.py
@@ -19,6 +19,32 @@ mPa = 0.00
 ev = 0.00
 mPv = 0.00
 
+dt = 0.1 #計測間隔
+x = np.array([[0.], [0.]]) # 初期速度を代入した「4次元状態」
+u = np.array([[0.], [0.]]) # 外部要素
+
+P = np.array([ [100., 0.], [0., 100.]]) # 共分散行列
+F = np.array([[1., 0.], [0., 1.]])  # 状態遷移行列
+H = np.array([[1., 0.], [0., 1.]])  # 観測行列
+R = np.array([[0.1, 0], [0, 0.1]]) #ノイズ
+I = np.identity((len(x)))	# 4次元単位行列
+
+def kalman_filter(x, P, vel):
+	# 予測
+	x = np.dot(F, x) + u
+	P = np.dot(np.dot(F, P), F.T)
+
+	# 計測更新
+	Z = np.array(vel)
+	y = Z.T - np.dot(H, x)
+	S = np.dot(np.dot(H, P), H.T) + R
+	K = np.dot(np.dot(P, H.T), np.linalg.inv(S))
+	x = x + np.dot(K, y)		
+	P = np.dot((I - np.dot(K, H)), P)
+
+	x = x.tolist()
+	P = P.tolist()
+	return x,P
 def accPID(Goal, bm, Kp, Ki, Kd, max, min):
 	bmx055data = BMX055.bmx055_read()
 	global ea, mPa
@@ -72,16 +98,17 @@ if __name__ == "__main__":
 		t2 = t1
 		t = 0.0
 		while t < 2.0:
-			velY = culvel(0.9, 1, t)
+			velY = culvel(0.5, 1, t)
 			#mp = velPID(vStraightGoal, velY, Kp, Ki, Kd, 60.0, 20.0)
 			velX = culvel(0.9, 0, t)
 			#mpL = velPID(10.0, velX, Kp, Ki, Kd, 20.0, 0.0)
+			v = kalman_filter(x, P, [velX,velY])
 			print("vY",velY,"vX",velX)
 			Motor.motor(30, 30)
 			#Motor.motor(mp + mpL, mp, 0.3)
 			t1 =time.time()
 			t = t1 - t2
-			Other.saveLog("logbmx.txt", t, BMX055.bmx055_read(), velY, velX)
+			Other.saveLog("logbmx.txt", t, BMX055.bmx055_read(), velY, velX, v[0], v[1])
 		Motor.motor(0, 0, 0.5)
 
 	except  KeyboardInterrupt:
