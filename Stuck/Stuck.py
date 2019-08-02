f8652d9546bcbf9a0926a7a4e2a706e001e514e5
Pasises <45708319+Pasises@users.noreply.github.com>
2019-08-03 00:20:56 +0900

    20190803

--- Stuck/Stuck.py
+++ Stuck/Stuck.py
@@ -5,17 +5,31 @@ sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/IM920')
 sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Motor')
 sys.path.append('/home/pi/git/kimuralab/Other')
 
+import math
 import pigpio
 import time
 import traceback
 
 import BMX055
 import Motor
-import Stuck
 
-if __name__ == "__main__":
-	try:
-		BMX055.bmx055_setup()
+stuck = 0
+
+def stuckDetection():
+    stuck = 0
+    bmx055data = BMX055.bmx055_read()
+
+    if(math.fabs(bmx055data[0]) > 1.0):
+        stuck = 1
+
+    # --- Return Value --- #
+    #   1: roll over
+    return stuck
+
+def stuckEscape(mode = 0):
+    # --- Mode --- #
+    #   1 : Roll Over
+    if(mode == 1):
 		flug = 0
 		count = 0
 		while flug <= 5:
@@ -29,12 +43,18 @@ if __name__ == "__main__":
 			Motor.motor(28, 28, 0.1)
 
 			# --- Roll Over Check --- #
-			if(Stuck.stuckDetection() == 1):
+			if(stuckDetection() == 1):
 				flug = flug + 1	#Not Roll Over
 			else:
 				flug = 0		#Roll Over
-			count = count - 1
-		Motor.motor(0, 0, 2)
+			count = count - 1 
+        Motor.motor(0, 0, 2)
+    else:
+        pass   
+
+if __name__ == "__main__":
+	try:
+        print(stuckDetection())
 	except:
 		Motor.motor(0, 0, 2)
 		Motor.motor_stop()
