33b60355995df81dd72f1235dbc50ba9d35b31b9
unknown <s-arai14@jcom.zaq.ne.jp>
2019-08-07 16:30:57 +0900

    20190807

--- Goal/Goal.py
+++ Goal/Goal.py
@@ -22,12 +22,12 @@ S_thd = 120	#Saturation threshold
 Kp = 0.8	#Gain proportional
 Ki = 0.7	#Gain integer
 Kd = 0.3	#Gain differential
-mpL = 10		#motor power for Low level
-mpH = 30	#motor power fot High level
-adj = 2		#adjust motor power
+mp_min = 10	#motor power for Low level
+mp_max = 30	#motor power fot High level
+mp_adj = 2		#adjust motor power
 
-def Togoal(photopath, H_min, H_max, S_thd, Kp, Ki, Kd, mpL, mpH):
-	global e, mP, bomb, adj
+def Togoal(photopath, H_min, H_max, S_thd, mp_min, mp_max, mp_adj):
+	global e, mP, bomb
 	Motor.motor(0,0,0.3)
 	Motor.motor(30,30,0.1)
 	Motor.motor(0,0,0.3)
@@ -37,39 +37,37 @@ def Togoal(photopath, H_min, H_max, S_thd, Kp, Ki, Kd, mpL, mpH):
 	print("bomb",bomb)
 	print("area",area)
 	if area == -1 and GAP == 0:
-		Motor.motor(30, 30 + adj, 0.3)
+		Motor.motor(30, 30 + mp_adj, 0.3)
 		Motor.motor(0, 0, 3)
 		return [0, area, GAP, photoname]
 
 	elif area == 0 and GAP == -1:
 		if bomb == 1:
-			Motor.motor(mpH, mpL + adj, 0.5, 2)
+			Motor.motor(mp_max, mp_min + mp_adj, 0.5, 2)
 			bomb = 1
 		else:
-			Motor.motor(mpL, mpH + adj, 0.5, 2)
+			Motor.motor(mp_min, mp_max + mp_adj, 0.5, 2)
 			bomb = 0
 
 		return [-1, area, GAP, photoname]
 
 	else:
 		if area < 10000 and area > 0 and GAP < 0:
-			#MP = velPID(0.0, -GAP, Kp, Ki, Kd, mpH + 10, mpH)
 			MP = curvingSwitch(GAP,10)
-			Motor.motor(mpH, mpH + MP + adj, 0.5)
+			Motor.motor(mp_max, mp_max + MP + mp_adj, 0.5)
 			bomb = 1
 		elif area < 10000 and area > 0 and GAP >= 0:
-			#MP = velPID(0.0, GAP, Kp, Ki, Kd, mpH + 10, mpH)
 			MP = curvingSwitch(GAP,10)
-			Motor.motor(mpH + MP, mpH, 0.5)
+			Motor.motor(mp_max + MP, mp_max + mp_adj, 0.5)
 			bomb = 0
 		elif area >= 10000 and GAP < 0:
 			MP = curvingSwitch(GAP,10)
-			Motor.motor(mpL, mpH + MP + adj, 0.3)
+			Motor.motor(mp_min, mp_max + MP + mp_adj, 0.3)
 			bomb = 1
 
 		elif area >= 10000 and GAP >= 0:
 			MP = curvingSwitch(GAP,10)
-			Motor.motor(mpH + MP, mpL, 0.3)
+			Motor.motor(mp_max + MP, mp_min + mp_adj, 0.3)
 			bomb = 0
 
 		else:
@@ -78,48 +76,6 @@ def Togoal(photopath, H_min, H_max, S_thd, Kp, Ki, Kd, mpL, mpH):
 		print('MP',MP)
 
 		return [1, area, GAP, photoname]
-
-def accPID(Goal, bm, Kp, Ki, Kd, max, min):
-	bmx055data = BMX055.bmx055_read()
-	global e, mP
-	e1 = e
-	e2 = e1
-	e = bmx055data[bm] - Goal
-	mP = mP + Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
-	print("bmx",bmx055data[bm])
-	mP = mP if mP <= max else max
-	if mP < 0:
-		mP = min
-	return mP
-
-def velPID(Goal, vel, Kp, Ki, Kd, max, min):
-	global e, mP
-	e1 = e
-	e2 = e1
-	e = vel - Goal
-	mP = mP + Kp * (e-e1) * 0.5 + Ki * e * 0.5 + Kd * ((e-e1) - (e1-e2) * 0.5 )
-	mP = mP if mP <= max else max
-	if mP < 0:
-		mP = min
-	return mP
-
-def culvel(fC, bm, t):
-	filterCoefficient = fC
-	lowpassValue = 0.0
-	highpassValue = 0.0
-	timeSpan = t
-	oldAccel = 0.0
-	vel = 0.0
-
-	bmx055data = BMX055.bmx055_read()
-	#lowpass
-	lowpassValue = lowpassValue * filterCoefficient + bmx055data[bm] * (1 - filterCoefficient)
-	#highpass
-	highpassValue = bmx055data[bm] - lowpassValue
-	#velcity
-	vel = ((highpassValue + oldAccel) * timeSpan) / 2 + vel
-	oldAccel = highpassValue
-	return vel
 
 def curvingSwitch(GAP, add):
 	if abs(GAP) > 144:
@@ -149,12 +105,12 @@ if __name__ == "__main__":
 	try:
 		GPS.openGPS()
 		BMX055.bmx055_setup()
-		goal = Togoal("photo/photo", H_min, H_max, S_thd, Kp, Ki, Kd, mpL, mpH)
+		goal = Togoal("photo/photo", H_min, H_max, S_thd, mp_min, mp_max, mp_adj)
 		while goal[0] != 0:
 			gpsData = GPS.readGPS()
-			goal = Togoal("photo/photo", H_min, H_max, S_thd, Kp, Ki, Kd, mpL, mpH)
+			goalFlug, goalArea, goalGAP, photoName  = Togoal("photo/photo", H_min, H_max, S_thd, mp_min, mp_max, mp_adj)
 			print("goal flug is",goal)
-			Other.saveLog("GoalLog.txt", time.time(), gpsData[1], gpsData[2], goal)
+			Other.saveLog("GoalLog.txt", time.time(), gpsData[1], gpsData[2], goalFlug, goalArea, goalGAP, photoName )
 			"""
 			count = 0
 			ahh = 0
