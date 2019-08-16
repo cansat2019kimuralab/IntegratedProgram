484f49fa5b95334e1832c0bec6df6dd563159c61
masatoshiMoto <k11000660@gmail.com>
2019-08-16 13:56:56 +0900

    20190816

--- Stuck/stuckEscape.py
+++ Stuck/stuckEscape.py
@@ -14,20 +14,42 @@ import Motor
 import Other
 
 
-def fuckingRun(mP, accy_thd):
-	Motor.motor(mP, mP, 3)
-	bmx055Data = BMX055.bmx055_read()
+def fuckingRun(mP, thd):
+	suminit = 0
+	count = 0
+	for i in range(5):
+		bmxinit = BMX055.bmx055_read()
+		print(i, "init:", bmxinit[0:6])
+		suminit = suminit + bmxinit[5]
+	aveinit = suminit / 5
+	print('aveinit', aveinit)
+	Motor.motor(-mP, mP, 1)
+	for i in range(20):
+		bmxnow = BMX055.bmx055_read()
+		print(i, "now:", bmxnow[0:6])
+		if bmxnow[5] - aveinit < thd:
+			count = count + 1
+			print('count', count)
+			if count > 5:
+				print("stack")
+				Motor.motor(0, 0, 2)
+				return 1
+		else:
+			count = 0
+	time.sleep(1)
 	Motor.motor(0, 0, 2)
-	if abs(bmx055Data[1]) < accy_thd:
-		Motor.motor(-mP, -mP, 3)
-		Motor.motor(mP, -mP, 3)
-		Motor.motor(-mP, mP, 3)
-		Motor.motor(0, 0, 2)
+	return 0
 
 if __name__ == "__main__":
 	try:
 		BMX055.bmx055_setup()
-		fuckingRun(30, 50)
+		stuckFlug = fuckingRun(50, 100)
+		if stuckFlug == 1:
+			Motor.motor(20, 20, 1)
+			Motor.motor(-20, -20, 1)
+			Motor.motor(20, -20, 1)
+			Motor.motor(-20, 20, 1)
+			Motor.motor(0, 0, 2)
 	except KeyboardInterrupt:
 		Motor.motor_stop()
 		print("Emergency!")
