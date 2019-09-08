cb59d3f0679093a27675f6ef22c702a6499f2e5f
Pasises <45708319+Pasises@users.noreply.github.com>
2019-09-08 03:12:02 -0700

    20190908

--- Calibration/Calibration.py
+++ Calibration/Calibration.py
@@ -36,50 +36,44 @@ def ellipse(B, x):
 	return ((x[0]/B[0])**2+(x[1]/B[1])**2-1.)
 
 def Calibration(path):
+	# - Read Data from Log - #
 	with open(path, "r") as f:
 		lines = f.readlines()
 		x_csv = []
 		y_csv = []
 		for line in lines:
 			word = line.split()
-			if(abs(float(word[0])) < 200 and abs(float(word[1])) < 250): 
+			if(math.fabs(float(word[0])) < 200 and math.fabs(float(word[1])) < 350): 
+				#print(word[0], word[1])
 				x_csv.append(float(word[0]))
 				y_csv.append(float(word[1]))
 
+	# - First Analysis - #
 	xx_csv = x_csv
 	yy_csv = y_csv
-
 	n = len(x_csv)
 	x2 = np.power(x_csv, 2)
 	y2 = np.power(y_csv, 2)
-
 	xy = 0.0
 	for i in range(len(x_csv)):
 		xy = xy + x_csv[i] * y_csv[i]
-
 	E = -(x_csv * (x2 + y2))
 	F = -(y_csv * (x2 + y2))
 	H = -(x2 + y2)
-
 	x_csv = np.sum(x_csv)
 	y_csv = np.sum(y_csv)
-
 	x2 = np.sum(x2)
 	y2 = np.sum(y2)
-
 	E = np.sum(E)
 	F = np.sum(F)
 	H = np.sum(H)
 	K = np.array([[x2,xy,x_csv], [xy,y2,y_csv], [x_csv,y_csv,n]])
 	L = np.array([E,F,H])
 	P = np.dot(np.linalg.inv(K),L)
-
 	x_ave = (-1/2)* P[0]
 	y_ave = (-1/2)* P[1]
-
 	x_csv = xx_csv - x_ave
 	y_csv = yy_csv - y_ave
-
 	xy_csv = np.array([x_csv, y_csv])
 
 	mdr = odr.Model(ellipse, implicit=True)
@@ -87,11 +81,56 @@ def Calibration(path):
 	myodr = odr.ODR(mydata, mdr, beta0=[1.,2.])
 	myoutput = myodr.run()
 
-	x_csv = x_csv * 7000.0 / myoutput.beta[1]
-	y_csv = y_csv * 7000.0 / myoutput.beta[0]
+	x_csv = x_csv / 70 * myoutput.beta[1]
+	y_csv = y_csv / 70 * myoutput.beta[0]
 
-	cal_data = [x_ave, y_ave, myoutput.beta[1] / 100, myoutput.beta[0] / 100]
+	cal_data = [x_ave, y_ave, 70.*myoutput.beta[1], 70.*myoutput.beta[0]]
 
+	# - Second Analysis - #
+	x_csv = []
+	y_csv = []
+	for i in range(len(xx_csv)):
+		r = math.sqrt((xx_csv[i] - x_ave)**2 + (yy_csv[i] - y_ave)**2)
+		#print(r)
+		if(r <= 140):
+			x_csv.append(xx_csv[i])
+			y_csv.append(yy_csv[i])
+	xx_csv = x_csv
+	yy_csv = y_csv
+	n = len(x_csv)
+	x2 = np.power(x_csv, 2)
+	y2 = np.power(y_csv, 2)
+	xy = 0.0
+	for i in range(len(x_csv)):
+		xy = xy + x_csv[i] * y_csv[i]
+	E = -(x_csv * (x2 + y2))
+	F = -(y_csv * (x2 + y2))
+	H = -(x2 + y2)
+	x_csv = np.sum(x_csv)
+	y_csv = np.sum(y_csv)
+	x2 = np.sum(x2)
+	y2 = np.sum(y2)
+	E = np.sum(E)
+	F = np.sum(F)
+	H = np.sum(H)
+	K = np.array([[x2,xy,x_csv], [xy,y2,y_csv], [x_csv,y_csv,n]])
+	L = np.array([E,F,H])
+	P = np.dot(np.linalg.inv(K),L)
+	x_ave = (-1/2)* P[0]
+	y_ave = (-1/2)* P[1]
+	x_csv = xx_csv - x_ave
+	y_csv = yy_csv - y_ave
+	xy_csv = np.array([x_csv, y_csv])
+
+	mdr = odr.Model(ellipse, implicit=True)
+	mydata = odr.Data(xy_csv,y=1)
+	myodr = odr.ODR(mydata, mdr, beta0=[1.,2.])
+	myoutput = myodr.run()
+
+	x_csv = x_csv / 70 * myoutput.beta[1]
+	y_csv = y_csv / 70 * myoutput.beta[0]
+
+	cal_data = [x_ave, y_ave, 70.*myoutput.beta[1], 70.*myoutput.beta[0]]
 	return cal_data
 
 def readDir(calData, bmx055data):
--- Calibration/calibration_test.py
+++ Calibration/calibration_test.py
@@ -21,44 +21,36 @@ def Calibration(path):
 		y_csv = []
 		for line in lines:
 			word = line.split()
-			if(math.fabs(float(word[0])) < 200 and math.fabs(float(word[1])) < 250): 
+			if(math.fabs(float(word[0])) < 200 and math.fabs(float(word[1])) < 350): 
+				#print(word[0], word[1])
 				x_csv.append(float(word[0]))
 				y_csv.append(float(word[1]))
 
 	xx_csv = x_csv
 	yy_csv = y_csv
-
 	n = len(x_csv)
 	x2 = np.power(x_csv, 2)
 	y2 = np.power(y_csv, 2)
-
 	xy = 0.0
 	for i in range(len(x_csv)):
 		xy = xy + x_csv[i] * y_csv[i]
-
 	E = -(x_csv * (x2 + y2))
 	F = -(y_csv * (x2 + y2))
 	H = -(x2 + y2)
-
 	x_csv = np.sum(x_csv)
 	y_csv = np.sum(y_csv)
-
 	x2 = np.sum(x2)
 	y2 = np.sum(y2)
-
 	E = np.sum(E)
 	F = np.sum(F)
 	H = np.sum(H)
 	K = np.array([[x2,xy,x_csv], [xy,y2,y_csv], [x_csv,y_csv,n]])
 	L = np.array([E,F,H])
 	P = np.dot(np.linalg.inv(K),L)
-
 	x_ave = (-1/2)* P[0]
 	y_ave = (-1/2)* P[1]
-
 	x_csv = xx_csv - x_ave
 	y_csv = yy_csv - y_ave
-
 	xy_csv = np.array([x_csv, y_csv])
 
 	mdr = odr.Model(ellipse, implicit=True)
@@ -71,8 +63,56 @@ def Calibration(path):
 
 	cal_data = [x_ave, y_ave, 70.*myoutput.beta[1], 70.*myoutput.beta[0]]
 
-	plt.scatter(xx_csv, yy_csv, c="blue")	#補正後
-	plt.scatter(x_csv, y_csv, c="red")		#補正前
+	x_csv = []
+	y_csv = []
+	for i in range(len(xx_csv)):
+		r = math.sqrt((xx_csv[i] - x_ave)**2 + (yy_csv[i] - y_ave)**2)
+		print(r)
+		if(r <= 140):
+			x_csv.append(xx_csv[i])
+			y_csv.append(yy_csv[i])
+
+	#print(x_csv)
+	#print(y_csv)
+	xx_csv = x_csv
+	yy_csv = y_csv
+	n = len(x_csv)
+	x2 = np.power(x_csv, 2)
+	y2 = np.power(y_csv, 2)
+	xy = 0.0
+	for i in range(len(x_csv)):
+		xy = xy + x_csv[i] * y_csv[i]
+	E = -(x_csv * (x2 + y2))
+	F = -(y_csv * (x2 + y2))
+	H = -(x2 + y2)
+	x_csv = np.sum(x_csv)
+	y_csv = np.sum(y_csv)
+	x2 = np.sum(x2)
+	y2 = np.sum(y2)
+	E = np.sum(E)
+	F = np.sum(F)
+	H = np.sum(H)
+	K = np.array([[x2,xy,x_csv], [xy,y2,y_csv], [x_csv,y_csv,n]])
+	L = np.array([E,F,H])
+	P = np.dot(np.linalg.inv(K),L)
+	x_ave = (-1/2)* P[0]
+	y_ave = (-1/2)* P[1]
+	x_csv = xx_csv - x_ave
+	y_csv = yy_csv - y_ave
+	xy_csv = np.array([x_csv, y_csv])
+
+	mdr = odr.Model(ellipse, implicit=True)
+	mydata = odr.Data(xy_csv,y=1)
+	myodr = odr.ODR(mydata, mdr, beta0=[1.,2.])
+	myoutput = myodr.run()
+
+	x_csv = x_csv / 70 * myoutput.beta[1]
+	y_csv = y_csv / 70 * myoutput.beta[0]
+
+	cal_data = [x_ave, y_ave, 70.*myoutput.beta[1], 70.*myoutput.beta[0]]
+
+	plt.scatter(xx_csv, yy_csv, c="blue")	#補正前
+	plt.scatter(x_csv, y_csv, c="red")		#補正後
 	plt.grid()
 	plt.axes().set_aspect('equal')
 	plt.show()
@@ -80,7 +120,7 @@ def Calibration(path):
 
 if __name__ == '__main__':
 	try:
-		file = 'calibrationLog0007.txt'
+		file = 'calibrationLog0000.txt'
 		cal_data = Calibration(file)
 		print(cal_data)
 	except KeyboardInterrupt:
